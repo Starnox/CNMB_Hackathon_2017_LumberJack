c744ca97822597c39d9dbaea7e499a11eb423897
xRoALex <bir.alex10@yahoo.ro>
2017-10-21 13:49:20 +0300

    e

--- Scrolling world/lumber.java
+++ Scrolling world/lumber.java
@@ -127,4 +127,7 @@ public class Lumber extends ScrollActor
          if(getOneIntersectingObject(vertical.class)!=null||getOneIntersectingObject(orizontal.class)==null)
         getWorld().moveCamera(-5);
     }
+    
+    int child()
+    {
 }
